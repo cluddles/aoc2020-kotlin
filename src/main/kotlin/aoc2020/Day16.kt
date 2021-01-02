54d7920f6c426ab69aa6db61bf0b030091a1fb77
Dan Fielding <dgfielding@gmail.com>
2021-01-02 20:05:09 +0000

    2020/16 solution - cleanup

--- src/main/kotlin/aoc2020/Day16.kt
+++ src/main/kotlin/aoc2020/Day16.kt
@@ -4,81 +4,73 @@ import java.io.File
 
 class Day16 {
 
-    data class Rule(val name: String) {
-        // Don't want hash etc to use these
-        val ranges: MutableList<IntRange> = ArrayList()
+    data class Rule(val name: String, val ranges: List<IntRange>) {
         val possibleIndexes: HashSet<Int> = HashSet()
-        override fun toString(): String {
-            return "Rule(name=$name, ranges=$ranges, possibleIndexes=$possibleIndexes)"
-        }
     }
     data class Ticket(val values: List<Int>)
     data class Notes(val rules: List<Rule>, val tickets: List<Ticket>)
 
+    // Rule format = rulename: i1-i2 or j1-j2
+    private fun parseRule(line: String) = line.split(": ").let { parts -> Rule(
+        parts[0],
+        parts[1].split(" or ").map { range ->
+            range.split("-").let { IntRange(it[0].toInt(), it[1].toInt()) }
+        }
+    )}
+
+    // Ticket format = x,y,z,...
+    private fun parseTicket(line: String) = Ticket(line.split(",").map { it.toInt() } )
+
     private fun parseNotes(src: File) : Notes {
-        val lines = src.readLines()
-        val emptyLines = lines.mapIndexedNotNull { i, line -> if (line.isBlank()) i else null }
-        return Notes(
-            // Rules
-            lines.slice(0 until emptyLines.first())
-                .map { line ->
-                    line.split(": ").let { sides ->
-                        val result = Rule(sides[0])
-                        result.ranges.addAll(
-                            sides[1].split(" or ").map { range ->
-                                range.split("-").let { bounds ->
-                                    IntRange(bounds[0].toInt(), bounds[1].toInt())
-                                }
-                            }
-                        )
-                        result
-                    }
-                },
-            // Tickets
-            lines.slice(emptyLines.first() + 2 until lines.size)
-                .filter { line -> line.contains(",") }
-                .map { line -> Ticket(line.split(",").map { it.toInt() } ) }
-        )
+        val rules = ArrayList<Rule>()
+        val tickets = ArrayList<Ticket>()
+        src.forEachLine { line ->
+            when {
+                line.contains("-") -> rules.add(parseRule(line))
+                line.contains(",") -> tickets.add(parseTicket(line))
+                else -> {}
+            }
+        }
+        return Notes(rules, tickets)
     }
 
-    private fun matchesRule(value: Int, rule: Rule) : Boolean = rule.ranges.any { it.contains(value) }
+    private fun ruleAccepts(rule: Rule, value: Int) : Boolean = rule.ranges.any { it.contains(value) }
 
     fun solvePart1(src: File) : Int {
         val notes = parseNotes(src)
         // Sum of all invalid values (those that can't match any rule)
-        return notes.tickets.drop(1)
-            .flatMap { ticket -> ticket.values }
-            .map { value -> if (notes.rules.none { rule -> matchesRule(value, rule) } ) value else 0 }
+        return notes.tickets.drop(1).flatMap { it.values }
+            .map { value -> if (notes.rules.none { rule -> ruleAccepts(rule, value) } ) value else 0 }
             .sum()
     }
 
     fun parseNotesAndIndex(src: File) : Notes {
         val notes = parseNotes(src)
-        notes.rules.forEach { rule -> rule.possibleIndexes.addAll(notes.tickets[0].values.indices) }
         // Phase 1 - strip invalid tickets
         val validTickets = notes.tickets.filter { ticket ->
             ticket.values.all { value ->
-                notes.rules.any { rule -> matchesRule(value, rule) }
+                notes.rules.any { rule -> ruleAccepts(rule, value) }
             }
         }
         // Phase 2 - use remaining (valid) tickets to determine possible indexes for each rule
+        notes.rules.forEach { rule -> rule.possibleIndexes.addAll(notes.tickets[0].values.indices) }
         validTickets.forEach { ticket ->
             ticket.values.forEachIndexed { i, value ->
                 notes.rules
-                    .filter { rule -> rule.possibleIndexes.contains(i) && !matchesRule(value, rule) }
+                    .filter { rule -> rule.possibleIndexes.contains(i) && !ruleAccepts(rule, value) }
                     .forEach { rule -> rule.possibleIndexes.remove(i) }
             }
         }
         // Phase 3 - reduce rule index mappings based on other rules needing the same index
-        val processed = HashSet<Rule>()
-        while (processed.size != notes.rules.size) {
-            val toProcess = notes.rules.filter { !processed.contains(it) && it.possibleIndexes.size <= 1 }
+        val processed = HashSet<String>()
+        do {
+            val toProcess = notes.rules.filter { !processed.contains(it.name) && it.possibleIndexes.size <= 1 }
             if (toProcess.isEmpty()) throw IllegalStateException("Could not reduce rule index mappings")
             toProcess.forEach { procRule ->
                 notes.rules.filter { it != procRule }.forEach { it.possibleIndexes.removeAll(procRule.possibleIndexes) }
             }
-            processed.addAll(toProcess)
-        }
+            processed.addAll(toProcess.map { it.name })
+        } while (processed.size != notes.rules.size)
         return notes;
     }
 
